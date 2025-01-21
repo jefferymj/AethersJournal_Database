28496a8f33bd7267474f9af05931ef2fdfd73b31
jefferymj <jefferymjoseph@gmail.com>
2025-01-21 02:39:15 -0800

    temp local push

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ public class Startup
             app.UseHsts();
         }
 
-        app.UseHttpsRedirection();
+        // app.UseHttpsRedirection();
         app.UseStaticFiles();
 
         app.UseRouting();
